928a5d138a9fb84c3b56b1c7e33540b2f77db0c8
adefemi <hardeyfehmie24@gmail.com>
2024-10-12 00:50:40 +0100

    hdvjh

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,13 +7,4 @@ from typing import Iterable, List, Sequence, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Computes the length of a list of sequences.
     '''
-    return [(i, len(i)) for i in lst]#!/usr/bin/env python3
-'''Task 9's module.
-'''
-from typing import Iterable, List, Sequence, Tuple
-
-
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    '''Computes the length of a list of sequences.
-    '''
     return [(i, len(i)) for i in lst]
